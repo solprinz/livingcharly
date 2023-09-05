64f90912ec01757416d47fc064cef8f7d995785b
Sol <solprinz@hotmail.com>
2023-09-05 19:09:09 -0300

    data-contacto

---
+++ src/components/dataContacto/DataContacto.jsx
@@ -0,0 +1,72 @@
+import "../../styles/styles.scss";
+import LogoFacebook from "/Logo Facebook.svg";
+import LogoIG from "/Logo IG.svg";
+import Ubicacion from "../../assets/Ubicacion.svg";
+import Horarios from "../../assets/Horarios.svg";
+import Correo from "../../assets/Correo.svg";
+import Whatsapp from "../../assets/Whatsapp.svg";
+import Telefono from "../../assets/Telefono.svg";
+
+export const DataContacto = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="data-contacto">
+          <div>
+            <div className="data-title">Sucursales</div>
+            <div className="data-padding">
+              <img src={Ubicacion} style={{ marginRight: "10px" }} />
+              Av. San Martín 2123, Mendoza
+            </div>
+            <div className="data-padding">
+              <img src={Ubicacion} style={{ marginRight: "10px" }} />
+              Beltrán 162, Mendoza
+            </div>
+
+            <div className="data-title">Horarios</div>
+            <div className="data-padding">
+              <img src={Horarios} style={{ marginRight: "10px" }} />
+              Lunes a viernes 9 a 16.30 hs
+            </div>
+            <div className="data-padding">
+              <img src={Horarios} style={{ marginRight: "10px" }} />
+              Sábados 10 a 13 hs
+            </div>
+          </div>
+
+          <div>
+            <div className="data-title">Contacto</div>
+            <div className="data-padding">
+              <img src={Telefono} style={{ marginRight: "10px" }} />
+              0261 425-0395
+            </div>
+            <div className="data-padding">
+              <img src={Whatsapp} style={{ marginRight: "10px" }} />
+              +54 9 2614 72-7258
+            </div>
+            <div>
+              <img src={Correo} style={{ marginRight: "10px" }} />
+              contacto@livingcharly.com
+            </div>
+          </div>
+          <div>
+            <div className="data-title">Nuestras redes</div>
+
+            <div>
+              <a href="https://wwww.facebook.com/">
+                <img
+                  src={LogoFacebook}
+                  alt=""
+                  style={{ padding: "12px 12px 12px 0px" }}
+                />
+              </a>
+              <a href="https://www.instagram.com">
+                <img src={LogoIG} alt="" style={{ padding: "12px" }} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
